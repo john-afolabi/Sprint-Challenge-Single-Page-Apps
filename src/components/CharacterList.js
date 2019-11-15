821d231071a8fc6517323e90c018e50933801708
John Afolabi <johnafolabi.da@gmail.com>
2019-11-15 10:00:16 +0100

    Display characters to dom with styles

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character/").then(res => {
+      console.log(res);
+      setCharacters(res.data.results);
+    });
+  }, []);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
@@ -9,8 +19,12 @@ export default function CharacterList() {
   }, []);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+    <section className="character-list d-flex flex-wrap">
+      {characters.map(char => {
+        return (
+          <CharacterCard char={char} key={char.id}/>
+        );
+      })}
     </section>
   );
 }
