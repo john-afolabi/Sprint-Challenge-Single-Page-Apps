c85a906b794e8f1aaca4117dd762e21abc288f24
John Afolabi <johnafolabi.da@gmail.com>
2019-11-15 10:34:29 +0100

    Implement search by name feature

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,30 +1,47 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredChar, setFilteredChar] = useState([]);
+
+  const searchHandler = e => {
+    setSearch(e.target.value);
+
+    setFilteredChar(
+      characters.filter(char => {
+        if (char.name.toLocaleLowerCase().indexOf(e.target.value.toLocaleLowerCase()) != -1) {
+          return char;
+        }
+      })
+    );
+  };
 
   useEffect(() => {
     axios.get("https://rickandmortyapi.com/api/character/").then(res => {
       console.log(res);
-      setCharacters(res.data.results);
+      setCharacters(res.data.results)
+      setFilteredChar(res.data.results);
     });
   }, []);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    
+  }, [])
 
   return (
-    <section className="character-list d-flex flex-wrap">
-      {characters.map(char => {
-        return (
-          <CharacterCard char={char} key={char.id}/>
-        );
-      })}
-    </section>
+    <div>
+      <h1>Rick and Morty Characters!</h1>
+      <SearchForm search={search} searchHandler={searchHandler}/>
+      <section className="character-list d-flex flex-wrap">
+        {filteredChar.map(char => {
+          return <CharacterCard char={char} key={char.id} />;
+        })}
+      </section>
+    </div>
   );
 }
